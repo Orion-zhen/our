040489a20dfaa9df4ef578d36fec49a3f7d5763a
Orion <128988082+Orion-zhen@users.noreply.github.com>
2025-02-01 23:36:28 +0800

    :boom: add yay to resolve aur dependency

--- scripts/build-one.sh
+++ scripts/build-one.sh
@@ -5,6 +5,9 @@ export repo=$1
 
 git clone "https://aur.archlinux.org/${repo}.git"
 cd "$ROOT_DIR/$repo"
+
+yay -S --asdeps --noconfirm $(grep -Po '(?<=^depends=\().*?(?=\))' PKGBUILD | tr -d "'")
+
 makepkg -scfL --noconfirm --noprogressbar --sign --key $GPG_SIG_KEY
 rm -rf *-debug-*
 
