08bb80dd794f7469c782e4b6facffa59790b9f2b
Orion <128988082+Orion-zhen@users.noreply.github.com>
2025-02-01 22:50:21 +0800

    :pencil2: typo GPG

--- scripts/build-one.sh
+++ scripts/build-one.sh
@@ -4,7 +4,7 @@ export ROOT_DIR=$(pwd)
 export repo=$1
 
 pacman-key --recv-keys $GPG_SIG_KEY
-pacman-key --lsign-key $PGP_SIG_KEY
+pacman-key --lsign-key $GPG_SIG_KEY
 
 git clone "https://aur.archlinux.org/${repo}.git"
 cd "$ROOT_DIR/$repo"
