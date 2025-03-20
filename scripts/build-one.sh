8798102d7471d224d295af08f281e416a068b234
Orion <128988082+Orion-zhen@users.noreply.github.com>
2025-03-20 19:50:55 +0800

    :art: better format

--- scripts/build-one.sh
+++ scripts/build-one.sh
@@ -4,7 +4,7 @@ export ROOT_DIR=$(pwd)
 export REPO=$1
 
 if ! test -f "$ROOT_DIR/$REPO/PKGBUILD"; then
-    git clone "https://aur.archlinux.org/${REPO}.git"
+    git clone "https://aur.archlinux.org/$REPO.git"
 fi
 cd "$ROOT_DIR/$REPO"
 
@@ -16,10 +16,10 @@ fi
 makepkg -scfL --noconfirm --noprogressbar --sign --key $GPG_SIG_KEY --skippgpcheck
 rm -rf *-debug-*
 
-if ! mv "$ROOT_DIR/$REPO/"*.pkg.tar.zst* $ROOT_DIR/ 2>/dev/null; then
+if ! mv $ROOT_DIR/$REPO/*.pkg.tar.zst* $ROOT_DIR/ 2>/dev/null; then
     echo "Package not found: $REPO"
     exit 1
 fi
 
 cd "$ROOT_DIR"
-rm -rf $ROOT_DIR/$REPO
+rm -rf "$ROOT_DIR/$REPO"
