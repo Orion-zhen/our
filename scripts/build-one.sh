b4fd3602fd36020fa43a4eb72a61a26bc5d78234
Orion <128988082+Orion-zhen@users.noreply.github.com>
2025-03-20 19:29:31 +0800

    :pencil2: variable name

--- scripts/build-one.sh
+++ scripts/build-one.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export ROOT_DIR=$(pwd)
-export repo=$1
+export REPO=$1
 
-if ! test -f "$ROOT_DIR/$repo/PKGBUILD"; then
+if ! test -f "$ROOT_DIR/$REPO/PKGBUILD"; then
     git clone "https://aur.archlinux.org/${repo}.git"
 fi
-cd "$ROOT_DIR/$repo"
+cd "$ROOT_DIR/$REPO"
 
 if command -v yay &> /dev/null; then
     yay -S --noconfirm $(grep -Po '(?<=^depends=\().*?(?=\))' PKGBUILD | tr -d "'")
@@ -16,10 +16,10 @@ fi
 makepkg -scfL --noconfirm --noprogressbar --sign --key $GPG_SIG_KEY --skippgpcheck
 rm -rf *-debug-*
 
-if ! mv "$ROOT_DIR/$repo/"*.pkg.tar.zst* $ROOT_DIR/ 2>/dev/null; then
-    echo "Package not found: $repo"
+if ! mv "$ROOT_DIR/$REPO/"*.pkg.tar.zst* $ROOT_DIR/ 2>/dev/null; then
+    echo "Package not found: $REPO"
     exit 1
 fi
 
 cd "$ROOT_DIR"
-rm -rf $ROOT_DIR/$repo
+rm -rf $ROOT_DIR/$REPO
